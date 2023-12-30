4aef5a86ceb7599d6ff3145e791c665a27468ba7
tanvir35web <islammdtanvirul@gmail.com>
2023-12-31 00:19:58 +0600

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Profile from './components/Profile';
 import SocialMediaSlider from './components/SocialMediaSlider';
-import AppLogo from './components/appLogo';
+import AppLogo from './components/AppLogo';
 import ContactsDetails from './components/contacts/ContactsDetails';
 import Footer from './components/footer/Footer';
 
